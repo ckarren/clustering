c15b16ad5e9c421e1b1df0b20ca3b5d7b7bcfb09
ckarren <ckarren@ncsu.edu>
2024-06-05 21:29:12 -0400

    update utils

--- reg.py
+++ reg.py
@@ -1,18 +1,31 @@
-from __future__ import print_function
-from statsmodels.compat import lzip
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from statsmodels.formula.api import ols
-import math
+import utils as ut 
+import os
 
+n_sample = 250
 
-data = pd.read_pickle('reg_data_500_with_dummies.pkl')
-y = np.asarray(data['logQ'])
+data_file = f'reg_data_{n_sample}_with_dummies.pkl'
+if os.path.exists(data_file):
+    data_file = data_file
+else:
+    ut.prepare_regression(sample=True, n_sample=n_sample)
+    ut.add_dummies('reg_data_' + str(n_sample) + '.pkl')
+    data_file = data_file
+#  data_file = 'reg_data_with_dummies.pkl'
+data = pd.read_pickle(data_file)
+data.dropna(axis=0, how='any', inplace=True, ignore_index=True)
+for i in data:
+    s = data[i].isna().sum()
+    if s != 0:
+        print(s)
+y = np.asarray(data['logQ']  )
 #  X = np.asarray(data.loc[:, data.columns != 'logQ'])
+#
 X = np.asarray(data['logP'])
-X = sm.add_constant(X)
-model = sm.OLS(y, X)
+X = sm.add_constant(X, prepend=False)
+model = sm.OLS(y, X, missing='raise')
 results = model.fit()
+print(results.summary())
 
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 
 import glob
 import numpy as np
+rng = np.random.default_rng(1234)
 from scipy import stats
 import matplotlib.pyplot as plt
 import pandas as pd 
+
 
 def tot_col(dfx):
     dfx['Total'] = dfx.sum(axis=1) 
@@ -439,12 +441,12 @@ def compare_radius_means(n_clusters, radii=['r1', 'r2', 'r3', 'r4', 'r5']):
     plt.show()
 
 
-def prepare_regression():
+def prepare_regression(sample=True, **kwargs):
+
     year1 = '../InputFiles/y1_SFR_hourly.pkl'
     use_y1 = pd.read_pickle(year1)
     df_y1 = pd.DataFrame(use_y1)
     df_y1 = clean_outliers(df_y1)
-
     year2 = '../InputFiles/y2_SFR_hourly.pkl'
     use_y2 = pd.read_pickle(year2)
     df_y2 = pd.DataFrame(use_y2)
@@ -453,47 +455,50 @@ def prepare_regression():
     bill = pd.read_pickle('../InputFiles/bill_all.pkl')
     df_bill = pd.DataFrame(bill)
     df_bill.index = df_bill.index.map(str)
-    
-    #  summer_wd = weekdays.loc[weekdays.index.month.isin(summer)].copy()
-    p1y1 = df_y1.loc[df_y1.index.month.isin([7, 8])].sum().apply(np.log)
-    p2y1 = df_y1.loc[df_y1.index.month.isin([9, 10])].sum().apply(np.log)
-    p3y1 = df_y1.loc[df_y1.index.month.isin([11, 12])].sum().apply(np.log)
-    p4y1 = df_y1.loc[df_y1.index.month.isin([1, 2])].sum().apply(np.log)
-    p5y1 = df_y1.loc[df_y1.index.month.isin([3, 4])].sum().apply(np.log)
-    p6y1 = df_y1.loc[df_y1.index.month.isin([5, 6])].sum().apply(np.log)
 
-    p1y2 = df_y2.loc[df_y2.index.month.isin([7, 8])].sum().apply(np.log)
-    p2y2 = df_y2.loc[df_y2.index.month.isin([9, 10])].sum().apply(np.log)
-    p3y2 = df_y2.loc[df_y2.index.month.isin([11, 12])].sum().apply(np.log)
-    p4y2 = df_y2.loc[df_y2.index.month.isin([1, 2])].sum().apply(np.log)
-    p5y2 = df_y2.loc[df_y2.index.month.isin([3, 4])].sum().apply(np.log)
-    p6y2 = df_y2.loc[df_y2.index.month.isin([5, 6])].sum().apply(np.log)
+    #  summer_wd = weekdays.loc[weekdays.index.month.isin(summer)].copy()
+    p1y1 = df_y1.loc[df_y1.index.month.isin([7,
+                                             8])].sum().apply(np.log).round(3)
+    p2y1 = df_y1.loc[df_y1.index.month.isin([9, 10])].sum().apply(np.log).round(3)
+    p3y1 = df_y1.loc[df_y1.index.month.isin([11, 12])].sum().apply(np.log).round(3)
+    p4y1 = df_y1.loc[df_y1.index.month.isin([1, 2])].sum().apply(np.log).round(3)
+    p5y1 = df_y1.loc[df_y1.index.month.isin([3, 4])].sum().apply(np.log).round(3)
+    p6y1 = df_y1.loc[df_y1.index.month.isin([5, 6])].sum().apply(np.log).round(3)
+
+    p1y2 = df_y2.loc[df_y2.index.month.isin([7, 8])].sum().apply(np.log).round(3)
+    p2y2 = df_y2.loc[df_y2.index.month.isin([9, 10])].sum().apply(np.log).round(3)
+    p3y2 = df_y2.loc[df_y2.index.month.isin([11, 12])].sum().apply(np.log).round(3)
+    p4y2 = df_y2.loc[df_y2.index.month.isin([1, 2])].sum().apply(np.log).round(3)
+    p5y2 = df_y2.loc[df_y2.index.month.isin([3, 4])].sum().apply(np.log).round(3)
+    p6y2 = df_y2.loc[df_y2.index.month.isin([5, 6])].sum().apply(np.log).round(3)
 
     all_list = [p1y1, p2y1, p3y1, p4y1, p5y1, p6y1, p1y2, p2y2, p3y2, p4y2, p5y2, p6y2]
 
     for i, d in enumerate(all_list):
-        all_list[i] = pd.concat([d, df_bill.iloc[:,i].apply(np.log)], axis=1)
-        all_list[i]['period'] = str(i+1)
+        all_list[i] = pd.concat([d, df_bill.iloc[:,i].apply(np.log)], axis=1,
+                                join='inner')
+        all_list[i]['period'] = str(i % 6)
         all_list[i].columns = ['logQ', 'logP', 'period']
-
+    #
     q_all = pd.concat(all_list, axis=0, join='inner')
     q_all = q_all.reset_index(names='user')
-    print(len(pd.unique(q_all['user'])))
-
-    #  q_all.to_pickle('reg_data_500.pkl')
-prepare_regression()
+    q_all.dropna(axis=0, how='any', inplace=True, ignore_index=True)
+    if sample:
+        if not kwargs:
+            n_sample = 300
+        else:
+            n_sample = kwargs['n_sample']
+        x = rng.choice(pd.unique(q_all['user']), n_sample, replace=False, )
+        q_all = q_all.loc[q_all['user'].isin(x)]
+        q_all.to_pickle(f'reg_data_{n_sample}.pkl')
+    else:
+        q_all.to_pickle('reg_data_test.pkl')
 
 def add_dummies(file='reg_data.pkl'):
     file = file
     data = pd.read_pickle(
         file
     )
-    #  data = pd.get_dummies(
-        #  data = data,
-        #  columns=["user"],
-        #  drop_first=True,
-        #  dtype=float
-    #  )
     data = pd.get_dummies(
         data = data,
         columns=["user", "period"],
@@ -501,7 +506,6 @@ def add_dummies(file='reg_data.pkl'):
         dtype=float
     )
     data.to_pickle(f'{file[:-4]}_with_dummies.pkl')
-
 def users():
     users = pd.read_pickle('../InputFiles/user_ids.pkl')
     users = [int(x) for x in users]
