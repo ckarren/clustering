6d0693f9bb4c28a37cd4100f06f4cac96a7b6a35
ckarren <ckarren@ncsu.edu>
2024-06-15 14:17:18 -0400

    "update reg with IV2SLS"

--- reg.py
+++ reg.py
@@ -1,20 +1,29 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+from statsmodels.sandbox.regression.gmm import IV2SLS
 import utils as ut
 
 data_file = ut.load_reg_data()
 data = pd.read_pickle(data_file)
 data.drop(columns='P', inplace=True)
+print(data.head())
 #  data.dropna(axis=0, how='any', inplace=True, ignore_index=True)
 
-y = np.asarray(data['P_ave'])
-X = np.asarray(data.loc[:, data.columns != 'logQ'])
-
+y = np.asarray(data['Q'])
+X = np.assarray(data['P_ave'])
+inst = np.asarray(data.iloc[:,2:])
+#  X = np.asarray(data.loc[:, data.columns != 'logQ'])
+#
 #
 #  X = np.asarray(data['P_ave'])
-X = sm.add_constant(X, prepend=False)
-model = sm.OLS(y, X)
+inst = sm.add_constant(X, prepend=False)
+model = IV2SLS(y, X, inst)
 results = model.fit()
-results.summary().as_csv()
 print(results.summary())
+results.summary().as_csv()
+
+#  model = sm.OLS(y, X)
+#  results = model.fit()
+#  results.summary().as_csv()
+#  print(results.summary())
