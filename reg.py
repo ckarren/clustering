c9348116cefa3082f7f75d26cf007a0156fdfdbb
ckarren <ckarren@ncsu.edu>
2024-06-17 10:03:50 -0400

    "add two stage least squares"

--- reg.py
+++ reg.py
@@ -7,17 +7,17 @@ import utils as ut
 data_file = ut.load_reg_data()
 data = pd.read_pickle(data_file)
 data.drop(columns='P', inplace=True)
-print(data.head())
 #  data.dropna(axis=0, how='any', inplace=True, ignore_index=True)
 
 y = np.asarray(data['Q'])
-X = np.assarray(data['P_ave'])
+X = np.asarray(data['P_ave'])
 inst = np.asarray(data.iloc[:,2:])
 #  X = np.asarray(data.loc[:, data.columns != 'logQ'])
 #
 #
 #  X = np.asarray(data['P_ave'])
-inst = sm.add_constant(X, prepend=False)
+X = sm.add_constant(X)
+inst = sm.add_constant(inst)
 model = IV2SLS(y, X, inst)
 results = model.fit()
 print(results.summary())
