98a6d5cda23cdf5f8262de14e62c4eff399ddb3d
Cade Karrenberg <ckarren@ncsu.edu>
2024-06-20 10:56:57 -0400

    update

--- reg.py
+++ reg.py
@@ -9,21 +9,15 @@ import utils as ut
 start = time.perf_counter()
 data = pd.read_pickle('B:/LAP_inst_reg_data_with_dummies.pkl')
 load = time.perf_counter()
-data.to_stata('lap_inst_data.dta')
-col_names = data.columns.tolist()
-if os.path.exists('columns_reg.csv'):
-    pass
-else:
-    np.savetxt('columns_reg.csv', col_names, delimiter=",", fmt='% s')
 y = np.asarray(data['Q'], dtype=np.float32)
 X = np.asarray(data['P_ave'], dtype=np.float32)
 inst = np.asarray(data.iloc[:,2:], dtype=np.float32)
 
 X = sm.add_constant(X)
 inst = sm.add_constant(inst)
-model = IV2SLS(y, X, inst)
+# model = IV2SLS(y, X, inst)
 
-#  model = sm.OLS(X, inst)
+model = sm.OLS(X, inst)
 results = model.fit()
 end = time.time()
 end = time.perf_counter()
