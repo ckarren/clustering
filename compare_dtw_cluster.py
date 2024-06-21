4d19aafd9c005fd79e70549bc5898ec45667254c
ckarren <ckarren@ncsu.edu>
2024-06-21 09:52:38 -0400

    update dtw comp

--- compare_dtw_cluster.py
+++ compare_dtw_cluster.py
@@ -11,26 +11,28 @@ from tslearn.preprocessing import TimeSeriesScalerMeanVariance
 
 seed = 0
 np.random.seed(seed)
-#  n_sample = 100
-n_cluster = 4
+n_sample = 500
+#  n_cluster = 4
 n_init = 5
 max_iter_barycenter=20
-cluster_windows = [2,3]
-file_path = str('~/OneDrive - North Carolina State University/Documents/Clustering+Elasticity/InputFiles/')
+#  cluster_windows = [2,3]
+cluster_window = 1
+n_clusters = [2, 3, 4, 5, 6, 7, 8, 9]
+file_path = str('../InputFiles/')
 use_file = file_path + 'y1_SFR_hourly.pkl'
 
 use_df = pd.read_pickle(use_file)
 use_df = ut.clean_outliers(use_df)
-#  use_df = use_df.sample(n=n_sample, axis=1, random_state=1)
+use_df = use_df.sample(n=n_sample, axis=1, random_state=1)
 
-#  X1_train = ut.groupby_year(use_df)
-X1_train = ut.groupby_month(use_df)
+X1_train = ut.groupby_year(use_df)
+#  X1_train = ut.groupby_month(use_df)
 X1_train = X1_train.T
 X_train = to_time_series_dataset(X1_train)
 X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
 sz = X_train.shape[1]
 
-for cluster_window in cluster_windows:
+for n_cluster in n_clusters:
     begin = time.perf_counter()
     dba_km = TimeSeriesKMeans(n_clusters=n_cluster,
                               n_init=n_init,
@@ -54,7 +56,7 @@ for cluster_window in cluster_windows:
         plt.text(0.25, .95,'Cluster %d' % (yi + 1),
                  transform=plt.gca().transAxes)
         if yi==2:
-            plt.title(f'DTW on Scaled Data with Window={cluster_window}')
+            plt.title(f'DTW on Scaled Yearly Data with n_clusters={n_cluster}')
     #  plt.tight_layout()
 #  plt.show()
 #  dba_km = TimeSeriesKMeans(n_clusters=n_cluster,
@@ -97,14 +99,14 @@ for cluster_window in cluster_windows:
              #  transform=plt.gca().transAxes)
     #  if yi == 1:
         #  plt.title("DBA with n_init=10")
-    plt.savefig(f'{n_cluster}_clusters_DTW_scaled_r{cluster_window}.png')
+    plt.savefig(f'{n_cluster}_clusters_DTW_scaled_r1.png')
     df = pd.DataFrame(list(zip(list(use_df.columns), dba_km.labels_)),
                       columns=['User', 'DBA cluster'])
-    df.to_csv(f'{n_cluster}_DTW_results_scaled_r{cluster_window}_month.csv')
+    df.to_csv(f'{n_cluster}_DTW_results_scaled_r1.csv')
 
     end = time.perf_counter()
     total = (end - begin) / 60
     print(f'Clustering took {total} minutes to run')
-#  df2 = pd.DataFrame(list(zip(sil_coef, inertia)),
-                   #  columns=[])
-#  df2.to_csv('silhouette_score.csv')
+df2 = pd.DataFrame(list(zip(sil_coef, inertia)),
+                   columns=[])
+df2.to_csv('silhouette_score.csv')
