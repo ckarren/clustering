e24d83a09e3db94c8cec52afcbcdc7de36fa1fea
ckarren <ckarren@ncsu.edu>
2024-07-31 09:21:10 -0400

    "update utils"

--- cluster_summary.py
+++ cluster_summary.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import matplotlib.pyplot as plt
+import utils as ut
 #  from utils import cluster_lot
 
 n_clusters = 5
@@ -10,9 +11,19 @@ fontsize = 18
 
 #  lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
 #  atts = ['EffectiveYearBuilt', 'SQFTmain', 'Bedrooms', 'Bathrooms', 'TotalValue']
+cluster_file = f'../RadiusComps/{n_clusters}_DTW_results_scaled_r{radius}.csv'
+cluster_data = pd.read_csv(cluster_file, 
+                           usecols=[1,2], 
+                           index_col=0)
+cluster_df = pd.DataFrame(cluster_data)
+
 df_use1 = pd.read_pickle('../InputFiles/y1_SFR_hourly.pkl')
-df_use1 = clean_outliers(df_use)
-df_use2 = 
+df_use2 = pd.read_pickle('../InputFiles/y2_SFR_hourly.pkl')
+df_use = pd.concat([df_use1, df_use2], join='inner')
+df_use = ut.clean_outliers(df_use)
+df_use = df_use.T
+#  df_use = df_use.join(cluster_df, how='inner')
+print(cluster_df.index)
 #  lot_summary_stat = lot_df[atts].describe()
 #  lot_summary_stat.to_csv('all_summary_stats.csv')
 #  lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
