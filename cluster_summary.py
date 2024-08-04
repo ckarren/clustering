22f6112dfa44c4a1a90724a9c75c344ad3929a6d
ckarren <ckarren@ncsu.edu>
2024-08-04 12:18:05 -0400

    update

--- cluster_summary.py
+++ cluster_summary.py
@@ -3,13 +3,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import utils as ut
-#  from utils import cluster_lot
+from utils import cluster_lot
 
 n_clusters = 5
 radius = 1
 fontsize = 18
 
-#  lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
+lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
 #  atts = ['EffectiveYearBuilt', 'SQFTmain', 'Bedrooms', 'Bathrooms', 'TotalValue']
 cluster_file = f'../RadiusComps/{n_clusters}_DTW_results_scaled_r{radius}.csv'
 cluster_data = pd.read_csv(cluster_file, 
@@ -17,16 +17,15 @@ cluster_data = pd.read_csv(cluster_file,
                            index_col=0)
 cluster_df = pd.DataFrame(cluster_data)
 
-df_use1 = pd.read_pickle('../InputFiles/y1_SFR_hourly.pkl')
-df_use2 = pd.read_pickle('../InputFiles/y2_SFR_hourly.pkl')
-df_use = pd.concat([df_use1, df_use2], join='inner')
-df_use = ut.clean_outliers(df_use)
-df_use = df_use.T
+#  df_use1 = pd.read_pickle('../InputFiles/y1_SFR_hourly.pkl')
+#  df_use2 = pd.read_pickle('../InputFiles/y2_SFR_hourly.pkl')
+#  df_use = pd.concat([df_use1, df_use2], join='inner')
+#  df_use = ut.clean_outliers(df_use)
+#  df_use = df_use.T
 #  df_use = df_use.join(cluster_df, how='inner')
-print(cluster_df.index)
 #  lot_summary_stat = lot_df[atts].describe()
 #  lot_summary_stat.to_csv('all_summary_stats.csv')
-#  lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
+lot_df = cluster_lot(n_clusters=n_clusters, radius=radius)
 #  atts = ['EffectiveYearBuilt', 'SQFTmain', 'Bedrooms', 'Bathrooms', 'TotalValue']
 
 #  lot_summary_stat = lot_df[atts].describe()
@@ -42,9 +41,9 @@ print(cluster_df.index)
     #  fig.supylabel('Probability density')
     #  plt.show()
 
-#  grouped = lot_df.groupby('DBA cluster')[['EffectiveYearBuilt', 'SQFTmain',
-                                        #  'Bedrooms', 'Bathrooms',
-                                        #  'TotalValue']].agg(['count', 'max',
-                                                           #  'min', 'mean',
-                                                           #  'std'])
-#  grouped.to_csv('5_clusters_summary_stats.csv')
+grouped = lot_df.groupby('DBA cluster')[['EffectiveYearBuilt',
+                                         'SQFTmain',
+                                        'Bedrooms', 
+                                         'Bathrooms',
+                                        'TotalValue']].agg(['median'])
+grouped.to_csv('5_clusters_summary_stats_median.csv')
