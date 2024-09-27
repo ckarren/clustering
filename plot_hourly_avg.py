a28f651482992e8cd3b879e4bdb99311d098a573
ckarren <ckarren@ncsu.edu>
2024-09-27 09:06:18 -0400

    hourly

--- plot_hourly_avg.py
+++ plot_hourly_avg.py
@@ -15,23 +15,24 @@ df_use = pd.read_pickle(file)
 df_use = ut.clean_outliers(df_use)
 df_use_y = ut.groupby_year(df_use)
 df_use_m = ut.groupby_month(df_use)
-#  fig, ax = plt.subplots(
+fig, ax = plt.subplots()
         #  figsize=(24, 6),
         #  layout='constrained')
-total = df_use_y.sum(axis=1)
-average = df_use_y.mean(axis=1)
-total.to_csv('daily_total.csv')
-average.to_csv('daily_average.csv')
+total = df_use_y.sum(axis=1).multiply(7.48)
+average = df_use_y.mean(axis=1).multiply(7.48)
+
+#  total.to_csv('daily_total.csv')
+#  average.to_csv('daily_average.csv')
         #  for i in df_use_rc.columns:
             #  i = str(i)
             #  ax.plot(df_use_rc.index, df_use_rc[i], c='grey')
-#  ax.plot(df_use_y.index, total, c='crimson')
+ax.plot(df_use_y.index, average, marker='o', c='mediumblue', linewidth=3)
         #  ax[ci].set_title(f'Average Use for Cluster {c}, radius {r}', fontsize=14)
         #  axs[ci].set_xlabel('Time (hr)', fontsize=fontsize)
         #  axs[ci].set_ylabel('Volume (gallons)', fontsize=fontsize)
-#  ax.set_xlabel('Time (hr)', fontsize=fontsize)
-#  ax.set_ylabel('Volume (gallons)', fontsize=fontsize)
-#  plt.show()
+ax.set_xlabel('Time (hr)', fontsize=fontsize)
+ax.set_ylabel('Volume (gallons)', fontsize=fontsize)
+plt.show()
 
 
     #  plt.savefig(f'../{n_clusters}_clusters-means_{r}.png')
